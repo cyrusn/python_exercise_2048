4fe0f1fcb65fdacd04212d703eb0bc42cd475221
cyrusn <cyrusncy@gmail.com>
2018-10-28 22:10:51 +0800

    more functional programming style

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-#!/usr/local/bin/python3
+#!/usr/bin/env python3
 
 from game import Game
 from tkinter import Tk, Frame, Label, CENTER
-from time import sleep
+
+# we have class variables
 
 GRID_LEN = 4
 GRID_PADDING = 8
@@ -13,100 +14,112 @@ LABEL_HEIGHT = 2
 BACKGROUND_COLOR_GAME = "#92877d"
 BACKGROUND_COLOR_CELL_EMPTY = "#9e948a"
 BACKGROUND_COLOR_DICT = {
-    0: BACKGROUND_COLOR_CELL_EMPTY, 2: "#eee4da", 4: "#ede0c8", 8: "#f2b179",
-    16: "#f59563", 32: "#f67c5f", 64: "#f65e3b", 128: "#edcf72",
-    256: "#edcc61", 512: "#edc850", 1024: "#edc53f", 2048: "#edc22e"
+    0: BACKGROUND_COLOR_CELL_EMPTY,
+    2: "#eee4da",
+    4: "#ede0c8",
+    8: "#f2b179",
+    16: "#f59563",
+    32: "#f67c5f",
+    64: "#f65e3b",
+    128: "#edcf72",
+    256: "#edcc61",
+    512: "#edc850",
+    1024: "#edc53f",
+    2048: "#edc22e",
 }
 CELL_COLOR_DICT = {
-    0: BACKGROUND_COLOR_CELL_EMPTY, 2: "#776e65", 4: "#776e65", 8: "#f9f6f2",
-    16: "#f9f6f2", 32: "#f9f6f2", 64: "#f9f6f2", 128: "#f9f6f2",
-    256: "#f9f6f2", 512: "#f9f6f2", 1024: "#f9f6f2", 2048: "#f9f6f2"
+    0: BACKGROUND_COLOR_CELL_EMPTY,
+    2: "#776e65",
+    4: "#776e65",
+    8: "#f9f6f2",
+    16: "#f9f6f2",
+    32: "#f9f6f2",
+    64: "#f9f6f2",
+    128: "#f9f6f2",
+    256: "#f9f6f2",
+    512: "#f9f6f2",
+    1024: "#f9f6f2",
+    2048: "#f9f6f2",
 }
 
 FONT = ("Verdana", FONT_SIZE, "bold")
 
-KEY_QUIT = 'q'
-KEY_RESET = 'r'
-KEY_UP = 'Up'
-KEY_DOWN = 'Down'
-KEY_RIGHT = 'Right'
-KEY_LEFT = 'Left'
+KEY_QUIT = "q"
+KEY_RESET = "r"
+KEY_UP = "Up"
+KEY_DOWN = "Down"
+KEY_RIGHT = "Right"
+KEY_LEFT = "Left"
 
 
 class App:
     def __init__(self, font=FONT):
         super().__init__()
         self.font = font
-        self.master = Tk()
+        self.Tk = Tk()
         self.lift_window()
-        self.add_listen_key_event()
+        self.register_key_listeners()
         self.grid_cells = []
         self.init_grid()
         self.init_score_label()
         self.game = Game()
         self.commands = {
             KEY_RESET: self.game.reset,
-            KEY_QUIT: self.master.quit,
-            KEY_UP: self.game.move_up,
-            KEY_DOWN: self.game.move_down,
-            KEY_LEFT: self.game.move_left,
-            KEY_RIGHT: self.game.move_right,
+            KEY_QUIT: self.Tk.quit,
+            KEY_UP: self.game.tilt_up,
+            KEY_DOWN: self.game.tilt_down,
+            KEY_LEFT: self.game.tilt_left,
+            KEY_RIGHT: self.game.tilt_right,
         }
 
-        self.game_start()
-
     def run(self):
-        self.master.mainloop()
+        self.game_start()
+        self.Tk.mainloop()
 
     def lift_window(self):
-        self.master.title('2048')
-        self.master.lift()
-        self.master.attributes("-topmost", True)
+        self.Tk.title("2048")
+        self.Tk.lift()
+        self.Tk.attributes("-topmost", True)
 
     def game_start(self):
-        self.game.newTile()
-        self.update_cells_values()
+        self.game.start()
+        self.update_cells()
 
-    def add_listen_key_event(self):
-        self.master.bind("<Key>", self.listen_key)
+    def register_key_listeners(self):
+        self.Tk.bind("<Key>", self.handle_key)
 
-    def listen_key(self, e):
-        if self.game.over:
-            self.score_label.configure(
-                text='Game over, your final score is {}'.format(
-                    self.game.score)
-            )
-
+    def handle_key(self, e):
         if e.char in self.commands:
             self.commands[e.char]()
 
         if e.keysym in self.commands:
             self.commands[e.keysym]()
-            self.game.newTile()
-            self.update_cells_values()
+            self.update_cells()
 
-    def update_cells_values(self):
+        if self.game.over:
+            self.score_label.configure(
+                text="Game over, your final score is {}".format(self.game.score)
+            )
+
+    def update_cells(self):
         for i in range(GRID_LEN):
             for j in range(GRID_LEN):
                 cell_value = self.game.board[j][i]
                 self.grid_cells[j][i].configure(
-                    text='{context}'.format(
-                        context='' if cell_value == 0 else cell_value,
+                    text="{context}".format(
+                        context="" if cell_value == 0 else cell_value
                     ),
                     bg=BACKGROUND_COLOR_DICT[cell_value],
-                    fg=CELL_COLOR_DICT[cell_value]
+                    fg=CELL_COLOR_DICT[cell_value],
                 )
-        self.score_label.configure(text='Score: {}'.format(self.game.score))
+        self.score_label.configure(text="Score: {}".format(self.game.score))
 
     def init_score_label(self):
-        self.score_label = Label(self.master, text='Hello world')
+        self.score_label = Label(self.Tk, text="")
         self.score_label.grid()
 
     def init_grid(self):
-        background = Frame(
-            self.master,
-            bg=BACKGROUND_COLOR_GAME
-        )
+        background = Frame(self.Tk, bg=BACKGROUND_COLOR_GAME)
         background.grid()
         for row in range(GRID_LEN):
             grid_row = []
@@ -117,17 +130,12 @@ class App:
                     justify=CENTER,
                     font=self.font,
                     width=LABEL_WIDTH,
-                    height=LABEL_HEIGHT
+                    height=LABEL_HEIGHT,
                 )
-                cell.grid(
-                    row=row,
-                    column=column,
-                    padx=GRID_PADDING,
-                    pady=GRID_PADDING
-                )
+                cell.grid(row=row, column=column, padx=GRID_PADDING, pady=GRID_PADDING)
                 grid_row.append(cell)
             self.grid_cells.append(grid_row)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     App().run()
--- game.py
+++ game.py
@@ -1,89 +1,143 @@
 from random import choice
 
 
-class Game():
+class Game:
     """
-        Class Game is stored all methods require for the game.
+        Class `Game` stores all methods require for the game.
     """
+
+    @staticmethod
+    def _new_board():
+        return [[0 for i in range(4)] for j in range(4)]
+
+    @staticmethod
+    def _merge_tiles(row):
+        """
+            merge the first 2 neighboring tiles in a row with the same value
+            into a tile of their sum.
+            note: row is muted in place
+
+            >>> [2, 2, 4], False => [4, 4]
+            >>> [2, 2], False => [4]
+            >>> [4, 2], False => [4, 2]
+        """
+        score = 0
+        result = row[:]
+
+        # do nothing if the length of row has only zero or 1 element.
+        if len(result) < 2:
+            return score, result
+
+        for i in range(len(result) - 1):
+            if row[i] == row[i + 1]:
+                result[i] = row[i] + row[i + 1]
+                score += result[i]
+                result.pop(i + 1)
+                break
+        return score, result
+
+    @staticmethod
+    def _free_fall(board):
+        scores = 0
+        for row in board:
+            filteredList = [x for x in row if x != 0]
+            score, newRow = Game._merge_tiles(filteredList)
+
+            while len(newRow) < 4:
+                newRow.append(0)
+
+            scores += score
+
+            for i in range(4):
+                row[i] = newRow[i]
+        return (scores, board)
+
+    @staticmethod
+    def _transpose(board, rotation=90):
+        transposed = Game._new_board()
+
+        for i in range(4):
+            for j in range(4):
+                if rotation == 0:
+                    transposed[j][i] = board[j][i]
+                elif rotation == 90:
+                    transposed[j][i] = board[i][j]
+                elif rotation == 180:
+                    transposed[j][i] = board[j][3 - i]
+                elif rotation == 270:
+                    transposed[j][i] = board[3 - i][3 - j]
+
+        return transposed
+
+    @staticmethod
+    def new_tile(board):
+        zero_tiles = []
+        value = choice([2, 2, 2, 4])
+
+        for j, row in enumerate(board):
+            for i, col in enumerate(row):
+                if col == 0:
+                    zero_tiles.append((i, j))
+
+        tile = choice(zero_tiles)
+        return (tile, value)
 
     def __init__(self):
         self.reset()
 
     def reset(self):
-        self.board = [[0 for i in range(4)] for j in range(4)]
+        self.board = Game._new_board()
         self.over = False
         self.score = 0
 
-    def _add(self, row, reverse=False):
-        """
-            add method is a method for adding first 2 neighbors with same values
-            (except 0) in a row, and append 0 to the end of row.
-            e.g. [0, 2, 2, 4] => [0, 4, 4, 0]
-        """
-        if reverse:
-            row.reverse()
+    def start(self):
+        try:
+            tile, value = Game.new_tile(self.board)
+            self.board[tile[1]][tile[0]] = value
+        except Exception:
+            self.over = True
 
-        result = row[:]
-        for i in range(3):
-            if row[i] == row[i+1] and row[i] != 0:
-                result[i] = row[i] + row[i+1]
-                self.score += result[i]
-                result.pop(i+1)
-                result.append(0)
-                break
-        if reverse:
-            result.reverse()
-        for i in range(4):
-            row[i] = result[i]
+    def _tilt(self, rotation):
+        transposedBoard = Game._transpose(self.board, rotation=rotation)
+        score, updatedBoard = Game._free_fall(transposedBoard)
+        self.score += score
+        self.board = Game._transpose(updatedBoard, rotation=rotation)
+        self.start()
 
-    def _transpose(self):
-        transposedBoard = [[0 for j in range(4)] for i in range(4)]
-        for i in range(4):
-            for j in range(4):
-                transposedBoard[i][j] = self.board[j][i]
-        return transposedBoard
+    def tilt_left(self):
+        self._tilt(rotation=0)
 
-    def _sortBoard(self, board, reverse=False):
-        for row in board:
-            row.sort(key=lambda x: x == 0, reverse=reverse)
-            self._add(row, reverse=reverse)
-        return board
+    def tilt_right(self):
+        self._tilt(rotation=180)
 
-    def _move_vertical(self, up=True):
-        transposedBoard = self._transpose()
-        sortedTransposedBoard = self._sortBoard(
-            transposedBoard, reverse=not up)
-        for i in range(4):
-            for j in range(4):
-                self.board[j][i] = sortedTransposedBoard[i][j]
+    def tilt_up(self):
+        self._tilt(rotation=90)
 
-    def _move_horizontal(self, left=True):
-        self._sortBoard(self.board, reverse=not left)
+    def tilt_down(self):
+        self._tilt(rotation=270)
 
-    def _checkZeroTiles(self):
-        zeroTiles = []
-        for j, row in enumerate(self.board):
-            for i, col in enumerate(row):
-                if col == 0:
-                    zeroTiles.append((i, j))
-        return zeroTiles
 
-    def newTile(self):
-        zeroTiles = self._checkZeroTiles()
-        if len(zeroTiles) == 0:
-            self.gameOver = True
-            return
-        selectTile = choice(zeroTiles)
-        self.board[selectTile[1]][selectTile[0]] = choice([2, 2, 2, 4])
+if __name__ == "__main__":
+    board = [[i + 4 * j for i in range(4)] for j in range(4)]
 
-    def move_left(self):
-        self._move_horizontal(left=True)
+    def printBoard(board):
+        for i in board:
+            for j in i:
+                print("{:2}".format(j), end=" ")
+            print()
+        print()
 
-    def move_right(self):
-        self._move_horizontal(left=False)
+    game = Game()
 
-    def move_up(self):
-        self._move_vertical(up=True)
+    commands = {
+        "a": game.tilt_left,
+        "d": game.tilt_right,
+        "w": game.tilt_up,
+        "s": game.tilt_down,
+    }
 
-    def move_down(self):
-        self._move_vertical(up=False)
+    while not game.over:
+        command = input("Please choose direction up: w, down: s, left: a, right: f\n")
+        if command in commands:
+            commands[command]()
+        printBoard(game.board)
